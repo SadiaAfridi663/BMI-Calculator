45e9d3b2a52fdece63716d8a9d7ee3e96e4520bc
Sadia Afridi <sadiaafridi663@gmail.com>
2024-10-10 18:02:55 +0500

    Added validation for weight and height inputs with error messages.

--- index.js
+++ index.js
@@ -8,4 +8,34 @@ var calculateBtn = document.getElementById("calculatebtn")
 calculateBtn.addEventListener("click", function(){
     var weight = +weightInput.value
 var height = +heightInput.value
+
+if((!weightInput.value.trim() || !heightInput.value.trim()) || (height <= 0 || weight <= 0)){
+
+    console.log("or if")
+    if(!heightInput.value.trim()  ){
+        console.log("height if")
+        heightError.textContent = "please enter height " 
+    }
+    else if(height <= 0){
+        heightError.textContent = "height must be positive"
+    }
+    heightInput.addEventListener('input', function() {
+            console.log("height input")
+            heightError.textContent = "";
+    });
+    
+    if(!weightInput.value.trim() ){
+        console.log("weight if")
+        weightError.textContent = "please enter weight "
+    }
+    else if(weight <= 0){
+        console.log("weight else if")
+        weightError.textContent = "weight must be positive"
+    }
+
+    weightInput.addEventListener('input', function() {
+        console.log("weight input")
+        weightError.textContent = "";
+        });
+}
 })
