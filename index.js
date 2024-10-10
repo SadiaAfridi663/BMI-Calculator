6b687e0b44f3d3f8a3431ff004e8ff9b3416f087
Sadia Afridi <sadiaafridi663@gmail.com>
2024-10-10 18:00:51 +0500

    Added event listener for calculate button

---
+++ index.js
@@ -0,0 +1,11 @@
+const mainContainer = document.querySelector("div")
+const inputContainer = document.getElementById("inputs")
+const catagory = document.getElementById("catagory")
+const weightInput = document.getElementById("weight")
+const heightInput = document.getElementById("height")
+var calculateBtn = document.getElementById("calculatebtn")
+
+calculateBtn.addEventListener("click", function(){
+    var weight = +weightInput.value
+var height = +heightInput.value
+})
